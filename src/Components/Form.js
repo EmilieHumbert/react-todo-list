80cfa87b4b23b5bb365019fa3042c55222e0915f
emilie <emi.m.humbert@googlemail.com>
2020-07-08 19:04:06 +0100

    Create Form component

---
+++ src/Components/Form.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { generate as generateId } from "shortid";
+
+function Form({ list, setList }) {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList([...list, { id: generateId(), text: input, complete: false }]);
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="description"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <input type="submit" value="submit" />
+    </form>
+  );
+}
+
+export default Form;
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,30 +1,19 @@
 import React, { useState } from "react";
 import { generate as generateId } from "shortid";
 import List from "./List";
+import Form from "./Form";
 
 function Project() {
-  const [input, setInput] = useState("");
-  const [list, setList] = useState([{ id: generateId(), text: "Example todo", complete: false }]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setList([...list, { id: generateId(), text: input, complete: false }]);
-    setInput("");
-  };
+  const [list, setList] = useState([
+    { id: generateId(), text: "Example todo", complete: false },
+  ]);
 
   return (
     <div>
       <h2>To do:</h2>
       <List list={list} setList={setList} />
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="description"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <input type="submit" value="submit" />
-      </form>
+      <Form list={list} setList={setList} />
     </div>
   );
 }
