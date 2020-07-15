3fd87053e7c61ba717eb8e21fa5786e7756bc58d
emilie <emi.m.humbert@googlemail.com>
2020-07-15 10:48:51 +0100

    Add auto height to Paper

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import Projects from "./Components/Projects";
 import "./App.css";
 import { Container, Typography, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      height: "auto",
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <Container maxWidth="sm">
         <Typography variant="h2" component="h1" gutterBottom>
           To do:
         </Typography>
-        <Paper>
+        <Paper className={classes.root}>
           <Projects />
         </Paper>
       </Container>
