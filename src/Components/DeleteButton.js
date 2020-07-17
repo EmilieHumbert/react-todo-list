f381bf9f9cb7c89fa870dd240f782196bfd99b35
emilie <emi.m.humbert@googlemail.com>
2020-07-17 11:57:06 +0100

    Set active tab on project delete

--- src/Components/DeleteButton.js
+++ src/Components/DeleteButton.js
@@ -4,6 +4,10 @@ import { Delete as DeleteIcon } from "@material-ui/icons";
 
 function DeleteButton({ todo, setList, list, setActive }) {
   function handleDelete(todo) {
+    if (setActive) {
+      const index = list.indexOf(todo);
+      setActive(index > 1 ? index - 1 : 0);
+    }
     setList(list.filter((item) => item !== todo));
   }
   return (
